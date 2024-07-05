91ae1abd71f9361a5971b6697c1d1d2878e8615d
Yegoun_Lee <yeai829@ajou.ac.kr>
2024-07-05 23:01:25 +0900

    first commit

---
+++ baekjoon.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n[10];
+    for(int i = 0; i < 5; i++){
+        scanf("%d", &n[i]);
+    }
+    
+    int sum = 0;
+    for(int i = 0; i < 5; i++){
+        sum += n[i];
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
